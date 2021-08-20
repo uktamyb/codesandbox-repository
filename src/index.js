04ee380b4d94885f4d6686bdc3da1973c99b3822
uktamyb <67410434+uktamyb@users.noreply.github.com>
2021-08-21 01:43:09 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Best JavaScript libraries</h1>
+    <ul>
+      <li>ReactJS</li>
+      <li>AngularJS</li>
+      <li>VeuJS</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
